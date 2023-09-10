31ffc6d7ec441a561d19424d9be0a1b4ce59426e
Alina Pestova <ali.pestova@yandex.ru>
2023-09-10 21:03:36 +0300

    new conflict

--- src/main/java/training/Main.java
+++ src/main/java/training/Main.java
@@ -1,7 +1,7 @@
 package training;
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("It's bad branch!");
+        System.out.printf("It's a conflict branch!");
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
         }
