5ea5d5ca9feae0c4b58be4cc32fff6392bdb6404
Alina Pestova <ali.pestova@yandex.ru>
2023-09-10 21:06:38 +0300

    hello

--- src/main/java/training/Main.java
+++ src/main/java/training/Main.java
@@ -1,7 +1,7 @@
 package training;
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome people numb 2!");
+        System.out.printf("Hello");
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
         }
